4569a4a54290371294ef717a8f75d566978ce933
Myles Burkemper <myles.burkemper@gmail.com>
2020-12-27 19:24:25 -0600

    handlebars skeletons

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const htmlRoutes = require('./html');
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"></link>
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
