1971896d333a86706c329686e335e28e91bb3d79
Donovan Chavez <chavezdon17@gmail.com>
2020-12-30 19:10:49 -0600

    deleted node mod

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,63 @@
-INSERT INTO food (flavor)
-VALUES ("vanilla");
+INSERT INTO hotDogs (food)
+VALUES ("Andy Secular");
 
-INSERT INTO  (flavor)
-VALUES ("chocolate");
+INSERT INTO  hotDogs (food)
+VALUES ("The Burner");
 
-INSERT INTO (flavor)
-VALUES ("strawberry");
+INSERT INTO hotDogs (food)
+VALUES ("Philly Steak");
+
+INSERT INTO hotDogs (food)
+VALUES ("Chihuahua");
+
+INSERT INTO hotDogs (food)
+VALUES ("BLT Dog");
+
+INSERT INTO hotDogs (food)
+VALUES ("Kitchen Sink");
+
+INSERT INTO hotDogs (food)
+VALUES ("Bio Diesel");
+
+INSERT INTO hotDogs (food)
+VALUES ("Corn Dog");
+
+INSERT INTO hotDogs (food)
+VALUES ("Meat Candy");
+
+INSERT INTO drinks (drink)
+VALUES ("Coke Cola");
+
+INSERT INTO drinks (drink)
+VALUES ("Dr. Pepper");
+
+INSERT INTO drinks (drink)
+VALUES ("Sprite");
+
+INSERT INTO drinks (drink)
+VALUES ("Diet Coke");
+
+INSERT INTO drinks (drink)
+VALUES ("Diet Sprite");
+
+INSERT INTO drinks (drink)
+VALUES ("Bud-Light");
+
+INSERT INTO drinks (drink)
+VALUES ("IPA");
+
+INSERT INTO drinks (drink)
+VALUES ("Water");
+
+INSERT INTO drinks (drink)
+VALUES ("Milk");
+
+INSERT INTO IceCreams (IceCream)
+VALUES ("Vanilla");
+
+INSERT INTO IceCreams (IceCream)
+VALUES ("Chocolate");
+
+INSERT INTO IceCreams (IceCream)
+VALUES ("Strawberry");
+
